742e8886240a0e9ad24a93ab30837f6d31de335f
TakeaKSV <cesar_3141230079@utd.edu.mx>
2026-01-18 23:45:05 -0600

    cors de kk

--- cotizaciones-ia/backend/server.js
+++ cotizaciones-ia/backend/server.js
@@ -13,8 +13,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-
+const FRONTEND_URL = process.env.FRONTEND_URL;
 app.use(helmet({
   crossOriginEmbedderPolicy: false,
   contentSecurityPolicy: false
